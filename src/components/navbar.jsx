7a3cb7e587c9489605132d8b838023e93909fe93
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-08-17 01:25:26 +0200

    Final Commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
 
-
-class NavBar extends Component {
-    state = {  } 
-    render() { 
-        return (
+const NavBar = (props) =>{
+return (
 
     <nav className="navbar bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <span>{this.props.totalCount}</span>
-  </div>
-</nav>
-        );
-    }
-}
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <span>{props.totalCount}</span>
+        </div>
+    </nav>
+    );
+};
+
  
 export default NavBar;
